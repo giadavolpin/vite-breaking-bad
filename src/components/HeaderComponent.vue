6e534be83373e769575920b462d51fccbf7e29d0
giadavolpin <volpingiada@gmail.com>
2022-11-14 15:31:17 +0100

    main

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <nav>
+        <!-- icone -->
+        <h1>Breaking Bad Api</h1>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: HeaderComponent
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <button>Select category</button>
+    <div class="bianco">
+        <div class="bluscuro">
+            Found 62 characters
+        </div>
+        <div class="card">
+            : {{ }}
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: MainComponent
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
